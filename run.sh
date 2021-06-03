9b57f4b44f07adb54b5fed5aaef3dbd25d1ea52e
Jun FUJINO <j_fujino@fm.me.es.osaka-u.ac.jp>
2021-06-03 11:54:52 +0900

    modified:   run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 gfortran mk_pov.f90
 ./a.out
 povray +W3840 +H2880 +A0.001 0020000_isosurface_phi.pov
+# povray +W1920 +H1440 +A0.001 0020000_isosurface_phi.pov
+# povray 0020000_isosurface_phi.pov
+# povray +P 0020000_isosurface_phi.pov
 rm a.out
-#povray +W1920 +H1440 +A0.001 0020000_isosurface_phi.pov
-#povray 0020000_isosurface_phi.pov
